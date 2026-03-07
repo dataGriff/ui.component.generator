caf98648909acd88c2da84d4a1e959ef698b7d4c
dataGriff <griff182uk@yahoo.co.uk>
2026-03-07 17:13:15 +0000

    feat: enhance live preview functionality with new tabs and Babel transformation for React Native code

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ui.component.generator/',
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
   build: {
     rollupOptions: {
       output: {
